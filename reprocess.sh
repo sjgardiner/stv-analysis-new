6df1a34e0f0c665324d2c2a0a5b6c67646e826cf
Steven Gardiner <gardiner@fnal.gov>
2020-04-06 17:07:42 -0500

    Remove GENIE dumper tools (no longer needed, handled directly in a modified version of the NuCCanalyzer module), update uboonecode version in reprocess.sh

--- reprocess.sh
+++ reprocess.sh
@@ -2,7 +2,7 @@
 
 # Set up uboonecode (we'll get the version of GENIE that we need for free)
 source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
-setup uboonecode v08_00_00_34 -q e17:prof
+setup uboonecode v08_00_00_40 -q e17:prof
 
 rm -f *stv.root
 genie -l -b -q reproc.C
