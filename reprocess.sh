d82bd156fb10673da34b8cd2039a87e49f4d6268
Steven Gardiner <gardiner@fnal.gov>
2020-10-27 17:10:11 -0500

    Remove dependence on the "genie" script in reprocess.sh (the extra GENIE class dictionaries are no longer needed)

--- reprocess.sh
+++ reprocess.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Set up uboonecode (we'll get the version of GENIE that we need for free)
+# Set up uboonecode (we'll get ROOT set up for free)
 source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
 setup uboonecode v08_00_00_40 -q e17:prof
 
 rm -f *stv.root
-genie -l -b -q reproc.C
+root -l -b -q reproc.C
