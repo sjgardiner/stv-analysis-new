848fa3990ee21085408adb7108c683e32c0d0f30
Steven Gardiner <gardiner@fnal.gov>
2022-10-25 16:30:35 -0500

    Add a new class derived from SystematicsCalculator that evaluates a covariance matrix for the central-value prediction in each true bin. TODO: add proper handling of correlations in the MC statistical uncertainties on the true prediction

---
+++ TruthSystematicsCalculator.hh
@@ -0,0 +1,70 @@
+#pragma once
+
+// Standard library includes
+#include <algorithm>
+
+// STV analysis includes
+#include "SystematicsCalculator.hh"
+
+// Calculates covariance matrices describing the uncertainty on the central
+// value prediction for the true event counts
+class TruthSystematicsCalculator : public SystematicsCalculator {
+
+  public:
+
+    TruthSystematicsCalculator( const std::string& input_respmat_file_name,
+      const std::string& syst_cfg_file_name = "",
+      const std::string& respmat_tdirectoryfile_name = "" );
+
+    virtual double evaluate_observable( const Universe& univ, int true_bin,
+      int flux_universe_index = -1 ) const override;
+
+    virtual double evaluate_mc_stat_covariance( const Universe& univ,
+      int true_bin_a, int true_bin_b ) const override;
+
+    virtual double evaluate_data_stat_covariance( int true_bin_a,
+      int true_bin_b, bool use_ext ) const override;
+
+    inline virtual size_t get_covariance_matrix_size() const override
+      { return true_bins_.size(); }
+
+};
+
+TruthSystematicsCalculator::TruthSystematicsCalculator(
+  const std::string& input_respmat_file_name,
+  const std::string& syst_cfg_file_name,
+  const std::string& respmat_tdirectoryfile_name )
+  : SystematicsCalculator( input_respmat_file_name,
+  syst_cfg_file_name, respmat_tdirectoryfile_name )
+{
+
+}
+
+double TruthSystematicsCalculator::evaluate_observable( const Universe& univ,
+  int true_bin, int /*flux_universe_index*/ ) const
+{
+  double true_events = univ.hist_true_->GetBinContent( true_bin + 1 );
+  return true_events;
+}
+
+double TruthSystematicsCalculator::evaluate_mc_stat_covariance(
+  const Universe& univ, int true_bin_a, int true_bin_b ) const
+{
+  // TODO: revisit to get off-diagonal MC stat covariances
+  if ( true_bin_a != true_bin_b ) return 0.;
+
+  // ROOT histograms use one-based bin indices, so I correct for that here.
+  // Note that using the bin error (rather than the bin contents) enables a
+  // correct treatment for weighted events provided TH1::Sumw2() was called
+  // before filling the histogram.
+  double err = univ.hist_true_->GetBinError( true_bin_a + 1 );
+  double err2 = err * err;
+  return err2;
+}
+
+double TruthSystematicsCalculator::evaluate_data_stat_covariance(
+  int /*true_bin_a*/, int /*true_bin_b*/, bool /*use_ext*/ ) const
+{
+  // The predicted true event counts have no data stat uncertainty
+  return 0.;
+}
